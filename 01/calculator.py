79def3c31cf23c09c45ee98ad7f2e5b8f7deea94
Igor Delistiianov <deambass@gmail.com>
2026-02-03 20:36:42 +0100

    Add looped calculator

--- 01/calculator.py
+++ 01/calculator.py
@@ -29,3 +29,35 @@ while True:
     else:
         print("Некоректно введена відповідь. Роботу калькулятора завершено.")
         break
+
+#cycle = "y"
+
+#while cycle == "y" or cycle == "yes":
+
+    a = float(input("Введіть перше число: "))
+    operation = input("Введіть дію (+, -, *, /): ")
+    b = float(input("Введіть друге число: "))
+
+    if operation == "+":
+        print("Результат:", a + b)
+
+    elif operation == "-":
+        print("Результат:", a - b)
+
+    elif operation == "*":
+        print("Результат:", a * b)
+
+    elif operation == "/":
+        if b == 0:
+            print("Помилка: ділити на 0 не можна!")
+        else:
+            print("Результат:", a / b)
+
+    else:
+        print("Помилка: невідома дія")
+
+    cycle = input("Продовжити? (y/yes): ").lower()
+
+    if cycle not in ["y", "yes"]:
+        print("Некоректно введена відповідь. Роботу калькулятора завершено.")
+        break
